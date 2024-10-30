c4b151afe8b99ad06fa938a29cb7bd61a8e605f5
PJuanTuiran <juanfer20091403@gmail.com>
2024-10-29 23:16:56 -0500

    commit initial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+<<<<<<< HEAD
           if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
+=======
+>>>>>>> 53a09a9 (commit initial)
     }
 }
