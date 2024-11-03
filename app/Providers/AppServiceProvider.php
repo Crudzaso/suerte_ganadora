06a069d6107ba178cc3d7d4b63a76f8596776d37
Darkot1 <sebastian.serna.1998@gmail.com>
2024-11-03 08:28:14 -0500

    feat: discord message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,15 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-<<<<<<< HEAD
         //
           if($this->app->environment('production')) {
             \URL::forceScheme('https');
-=======
-        // Forzar HTTPS
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
->>>>>>> 1549efa (feat: Implementation of messages to discord)
         }
     }
 }
